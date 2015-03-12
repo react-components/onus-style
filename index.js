a6c86f67e2a66b2f89265edacf285707a6f7ab93
camshaft <bytheway.cameron@gmail.com>
2015-03-11 20:57:17 -0600

    use injected toCSS function

--- index.js
+++ index.js
@@ -3,30 +3,14 @@
  */
 
 var Onus = require('onus');
-var React = require('react');
 
-var componentCache = {};
-
-module.exports = function(conf) {
+module.exports = function(conf, toCSS) {
   var render = conf.render;
   return Onus({
     render: function(DOM, $get, props, state, _yield, params, query, forms, t) {
-      var out = render(format, $get, props, state, _yield, params, query, forms, t);
+      var out = render.call(this, toCSS, $get, props, state, _yield, params, query, forms, t);
       var style = Array.isArray(out) ? out.join('') : out;
       return DOM('style', null, '\n' + style);
     }
   });
-}
-
-function format(selectors, props) {
-  return selectors.join(',\n') + ' {\n' + formatProps(props) + '\n}\n';
-}
-
-function formatProps(props) {
-  var parts = [];
-  for (var k in props) {
-    if (k === 'key') continue;
-    parts.push('  ' + k + ':' + props[k] + ';');
-  }
-  return parts.join('\n');
-}
+};
